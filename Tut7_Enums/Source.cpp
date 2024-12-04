4b3417499cd0618479187916c24b45997de3fcae
Jake Dusing <dusingjake@gmail.com>
2024-12-03 19:02:03 -0600

    add enum practice

--- Tut7_Enums/Source.cpp
+++ Tut7_Enums/Source.cpp
@@ -3,8 +3,35 @@
 
 using namespace std;
 
+enum PlayerStatus
+{
+	PS_Crouched,  // defaults to 0
+	PS_Standing, // defaults to 1
+	PS_Walking, // 2
+	PS_Running //3
+};
+
+enum MovementStatus
+{
+	MS_Crouched,
+	MS_Running
+};
+
 int main()
 {
+	PlayerStatus status;
+	status = PlayerStatus::PS_Running;
+
+	if (status == PS_Crouched)
+	{
+		cout << "The player is crouching! \n" << endl;
+	}
+
+	status = PS_Walking;
+	if (status == PS_Crouched)
+	{
+		cout << "The player is crouching! \n" << endl;
+	}
 
 	system("pause");
 }
